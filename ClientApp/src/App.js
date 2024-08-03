6f4e37cd1d45de68a7cf2e68ee256cc97f8f835a
StanislavSlobodenyuk <116057509+StanislavSlobodenyuk@users.noreply.github.com>
2024-08-03 15:20:07 +0300

    Фіксація клієнтської частини

---
+++ ClientApp/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import TestApi from './TestApi';
+
+function App() {
+  return (
+      <div className="App">
+          <TestApi /> 
+      </div>
+  );
+}
+
+export default App;
