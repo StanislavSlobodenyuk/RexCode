2406ef8eb3bdbaaf5227c54c298e44499073acea
StanislavSlobodenyuk <116057509+StanislavSlobodenyuk@users.noreply.github.com>
2024-08-03 15:14:19 +0300

    Налаштували перший контролер для нашого webApi додатку + пыдключили CORS + налаштували строку підключення до бд

---
+++ RexCode/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var response = new { Message = "Сервер работает!" };
+            return Ok(response);
+        }
+    }
+}
